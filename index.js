eb8dc10f36dcc31f7af065072315b8c8d0c89f7c
Quang Nguyen <quang.sunlight@gmail.com>
2021-12-25 05:53:13 +0700

    Handle multiple content types

--- index.js
+++ index.js
@@ -17,15 +17,79 @@ const PORT = process.env.PORT || 3500;
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
-  if (req.url === '/' || req.url === 'index.html') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    let filePath = path.join(__dirname, 'src', 'views', 'index.html');
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      res.end(data);
-    });
+  const extension = path.extname(req.url);
+
+  let contentType;
+  switch (extension) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'image/jpeg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.txt':
+      contentType = 'text/plain';
+      break;
+    default:
+      contentType = 'text/html';
+      break;
+  }
+
+  let filePath = path.join(__dirname, 'src', req.url);
+  if (contentType === 'text/html') {
+    filePath =
+      req.url === '/'
+        ? path.join(__dirname, 'src', 'views', 'index.html')
+        : (contentType === req.url.slice(-1)) === '/'
+        ? path.join(__dirname, 'src', 'views', req.url, 'index.html')
+        : path.join(__dirname, 'src', 'views', req.url);
+  }
+
+  // make .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+  const fileExists = fs.existsSync(filePath);
+  if (fileExists) {
+    // serve the file
+    serveFile(filePath, contentType, res);
+  } else {
+    switch (path.parse(filePath).base) {
+      case 'old-page.html': // 301 redirect
+        res.writeHead(301, { Location: '/new-page.html' });
+        res.end();
+        break;
+      case 'www-page.html': // 301 redirect
+        res.writeHead(301, { Location: '/' });
+        res.end();
+        break;
+      default:
+        // 404 not found
+        serveFile(path.join(__dirname, 'src', 'views', '404.html'), 'text/html', res);
+        break;
+    }
+    console.log(path.parse(filePath));
   }
 });
 
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const data = await fsPromises.readFile(filePath, 'utf8');
+    response.writeHead(200, { 'Content-Type': contentType });
+    response.end(data);
+  } catch (err) {
+    console.log(err);
+    response.statusCode = 500;
+    response.end();
+  }
+};
+
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
- 
