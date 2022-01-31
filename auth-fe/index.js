fac8655d4205dc82b6ab33400f317813f51ccf13
Quang Nguyen <quang.sunlight@gmail.com>
2022-01-31 22:16:00 +0700

    Initializing auth front end

---
+++ auth-fe/index.js
@@ -0,0 +1,29 @@
+console.log('Authentication Front End');
+
+const sendLogin = async () => {
+  const user = 'walt1';
+  const pwd = 'Aa$12345';
+  try {
+    const response = await fetch('http://localhost:3500/auth', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      body: JSON.stringify({ user, pwd }),
+    });
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        return await sendRefreshToken();
+      }
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+    return await response.json();
+  } catch (err) {
+    console.log(err.stack);
+  }
+};
+
+(async () => {
+  console.log('Init Async');
+  await sendLogin();
+})();
