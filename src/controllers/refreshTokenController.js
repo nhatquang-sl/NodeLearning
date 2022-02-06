d3ba15132d16d9a419e700149c380852108229b3
Quang Nguyen <quang.sunlight@gmail.com>
2022-02-07 00:12:24 +0700

    Move dotenv to index.js

--- src/controllers/refreshTokenController.js
+++ src/controllers/refreshTokenController.js
@@ -7,7 +7,6 @@ const usersDB = {
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
 
 const handleRefreshToken = (req, res) => {
   const cookies = req.cookies;
